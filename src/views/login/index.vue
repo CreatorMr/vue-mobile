86236e3edac390ed0b678a63053e1f5200c4e80c
Creator <382988057@qq.com>
2020-12-23 01:26:24 +0800

    login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="login-container">登陆页面</div>
+<div class="login-container">
+  <van-nav-bar class="app-nav-bar" title="登录" left-arrow @click-left="$router.back()" />
+  <van-cell-group>
+    <van-field v-model="phone" icon-prefix="icon" left-icon="video" placeholder="请输入手机号" />
+    <van-field v-model="sms" center left-icon="smile-o" clearable label="短信验证码" placeholder="请输入短信验证码">
+      <template #button>
+        <van-button size="mini" class="send-btn" round type="primary">发送验证码</van-button>
+      </template>
+    </van-field>
+  </van-cell-group>
+  <div class="login-btn-wrap">
+    <van-button class="login-btn" type="info" block>登录</van-button>
+  </div>
+
+</div>
 </template>
 
 <script>
@@ -8,7 +22,10 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      phone: '',
+      sms: ''
+    }
   },
   computed: {},
   watch: {},
@@ -16,8 +33,30 @@ export default {
   mounted () {},
   methods: {}
 }
-
 </script>
 
-<style>
+<style lang="less" scoped>
+.login-container {
+  .send-btn{
+    width: 76px;
+    height: 23px;
+    background-color: #ededed;
+    border: none;
+   .van-button__text{
+      color: #666666;
+    font-size: 11px;
+   }
+  }
+  .login-btn-wrap {
+    padding: 26px 16px;
+
+    .login-btn {
+      background: #6db4fb;
+      border: none;
+      .van-button__text{
+        font-size: 15px;
+      }
+    }
+  }
+}
 </style>
